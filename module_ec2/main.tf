3b813fbb334f2e372df9155691865a5ad4eb0fe2
AnugetHub1627 <anummore27@gmail.com>
2026-05-15 01:34:31 -0700

    updated

--- module_ec2/main.tf
+++ module_ec2/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "app_server" {
-    ami = var.ami_id
+    ami = var.ami
     instance_type = var.instance_type
     associate_public_ip_address = true 
     key_name = "key_anu"
     security_group_ids = ["sg-026d9aee394f5d5b1"]
     tags = {
-        Name = var.instance_name 
+        Name = var.name 
         owner = "anuradha"
         env = "prod"
     }
